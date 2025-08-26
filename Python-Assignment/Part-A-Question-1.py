78b1c54098ecf7c318618b36f6798d1503ea115b
Abhishek28-st <abhishek@eazydiner.com>
2025-08-26 16:29:55 +0530

    Correcting the code

--- Python-Assignment/Part-A-Question-1.py
+++ Python-Assignment/Part-A-Question-1.py
@@ -3,4 +3,4 @@ if len(user_string) > 0:
     first_character = user_string[0]
     print(f"The first character of your string is: {first_character}")
 else:
-    print("You entered an empty string, so there is no first character to diplay.")
+    print("You entered an empty string, so there is no first character to display.")
