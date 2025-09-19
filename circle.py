eb67200b9f02f11fdf95c432f23e41d0c3ca43e0
Abhishek28-st <abhishek@eazydiner.com>
2025-09-19 19:54:38 +0530

    Generating Circle using Mid-Point Circle Drawing Algorithm

--- circle.py
+++ circle.py
@@ -1,23 +1,45 @@
+import matplotlib.pyplot as plt
+
 def midpoint_circle(r):
-    x = 0 
+    x = 0
     y = r
-    r = 10
     p = 1 - r
-    points = [] 
+    points = []
+
     while x <= y:
-        points.append((x, y))
-        points.append((y, x))
-        points.append((x, -y))
-        points.append((y, -x))
-        points.append((-x, y))
-        points.append((-y, x))
+        points.append(( x,  y))
+        points.append(( y,  x))
+        points.append(( x, -y))
+        points.append(( y, -x))
+        points.append((-x,  y))
+        points.append((-y,  x))
         points.append((-x, -y))
         points.append((-y, -x))
+
         if p < 0:
-            p += 2 * x + 1
+            p = p + 2*x + 1
         else:
-            p += 2 * (x - y) + 1
+            p = p + 2*(x - y) + 1
             y -= 1
         x += 1
+
+    points = list(set(points))
     return points
-print(midpoint_circle(10))
+
+
+r = 1000
+points = midpoint_circle(r)
+
+xs = [p[0] for p in points]
+ys = [p[1] for p in points]
+
+plt.figure(figsize=(6, 6))
+plt.scatter(xs, ys, s=30, color="blue")  # draw points
+plt.gca().set_aspect('equal', adjustable='box')
+plt.title(f'Midpoint Circle Algorithm (r = {r})')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid(True)
+
+plt.savefig("circle.png", bbox_inches="tight")
+plt.show()
